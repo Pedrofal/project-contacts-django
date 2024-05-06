6a561206909c32ba997ae933d094362ccc7fc77d
Pedro Falcão <pedrofalcao21@gmail.com>
2024-05-06 16:44:12 -0300

    Add models

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name','id', 'last_name', 'phone',
+    ordering = '-id',
+    # list_filter = ','
+    search_fields = 'id','first_name', 'last_name', 'phone',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_display_links = 'first_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+   
+
+
+
