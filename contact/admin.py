146970c91c0ee216ebbefe6fce7ce9fa6a619839
Pedro Falcão <pedrofalcao21@gmail.com>
2024-05-14 14:54:58 -0300

    Add single contact page and partials html

--- contact/admin.py
+++ contact/admin.py
@@ -3,10 +3,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'first_name','id', 'last_name', 'phone',
+    list_display = 'first_name','id', 'last_name', 'phone', 'show',
     ordering = '-id',
     list_filter = 'created_date', 
     search_fields = 'id','first_name', 'last_name', 'phone',
+    list_editable = 'show',
     list_per_page = 10
     list_max_show_all = 100
     list_display_links = 'first_name',
