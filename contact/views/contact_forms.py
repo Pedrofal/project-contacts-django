2f50aedb02916c8d49dcec743621e1c51349f334
Pedro Falcão <pedrofalcao21@gmail.com>
2024-05-15 10:11:32 -0300

    Developing create contact feature

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404, redirect  
+from django.core.paginator import Paginator
+from contact.models import Contact
+from contact.forms import ContactForm
+from django.db.models import Q
+
+
+
+
+def create(request):
+    if request.method == 'POST':
+       
+        context = {
+            'form': ContactForm(request.POST)
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+    
+    context = {
+            'form': ContactForm()
+            }
+    return render(
+            request,
+            'contact/create.html',
+            context,
+            )
